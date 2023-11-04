60469c0f387d3307e54ffa1486237a4f8ea6295b
demiavaliani <demikoavaliani@gmail.com>
2023-11-04 21:30:44 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+	<ComingSoon />
+</template>
+
+<script setup lang="ts">
+	import ComingSoon from './views/ComingSoon.vue';
+</script>
+
+<style lang="scss"></style>
---
+++ src/views/ComingSoon.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="coming-soon">
+		<p>WE'RE COMING SOON</p>
+	</div>
+</template>
+
+<style lang="scss" scoped>
+	.coming-soon {
+		height: 100%;
+	}
+
+	p {
+		position: relative;
+		top: 35%;
+		font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+		font-size: 10rem;
+		font-weight: 600;
+	}
+</style>
