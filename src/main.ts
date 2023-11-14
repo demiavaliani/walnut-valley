1c6d8f25b740608fb33c00707d7ce9c3ad7f2898
demiavaliani <demikoavaliani@gmail.com>
2023-11-14 22:11:56 +0200

    fix: proper global styles setup

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
-import './styles/app.scss';
-
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 
 import App from './App.vue';
 import router from './router';
+
+import './styles/app.scss';
 
 const app = createApp(App);
 
