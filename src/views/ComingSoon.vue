7cb9b8651265bd06eb26b0a07d1c867f5e7dccdd
demiavaliani <demikoavaliani@gmail.com>
2023-11-14 22:12:51 +0200

    feat(ComingSoon): add logo

--- src/views/ComingSoon.vue
+++ src/views/ComingSoon.vue
@@ -1,19 +1,28 @@
 <template>
 	<div class="coming-soon">
+		<img src="../assets/images/logo.png" />
 		<p>WE'RE COMING SOON</p>
 	</div>
 </template>
 
 <style lang="scss" scoped>
 	.coming-soon {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
 		height: 100%;
+		gap: 10rem;
+	}
+
+	img {
+		width: 20vw;
 	}
 
 	p {
-		position: relative;
-		top: 35%;
 		font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
 		font-size: 5vw;
 		font-weight: 600;
+		color: $wv-logo-kernel-color;
 	}
 </style>
