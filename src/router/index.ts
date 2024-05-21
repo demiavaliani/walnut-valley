87e325da4a89dfd6e9324efa71fa1ca0eaeddfe7
demiavaliani <demikoavaliani@gmail.com>
2024-05-22 01:22:34 +0400

    feat(Production): component

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import { Views } from '@/constants';
 import { MainView, OurCompany, ContactUs, Production } from '@/views';
 
@@ -8,6 +8,7 @@ const routes = [
 		name: Views.MAIN,
 		component: MainView,
 	},
+
 	{
 		path: '/our-company',
 		name: Views.OUR_COMPANY,
@@ -26,7 +27,7 @@ const routes = [
 ];
 
 const router = createRouter({
-	history: createWebHistory(import.meta.env.BASE_URL),
+	history: createWebHashHistory(import.meta.env.BASE_URL),
 	routes,
 });
 
